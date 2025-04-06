10049c3fe66c86893d04844a34c770b9887e1b0c
gabriel <gabrielmesquita0503@gmail.com>
2025-04-06 15:27:03 -0300

    My Applications: Cancelling Applications

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -2,27 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\JobApplication;
 
 class MyJobApplicationController extends Controller
 {
     public function index()
     {
-        return view(
-            'my_job_application.index',
-            [
-                'applications' => auth()->user()->jobApplications()
-                    ->with([
-                        'job' => fn($query) => $query->withCount('jobApplications')
-                            ->withAvg('jobApplications', 'expected_salary'),
-                        'job.employer'
-                    ])->latest()->get()
-            ]
-        );
+        $applications = auth()->user()->jobApplications()
+            ->with([
+                'job' => function ($query) {
+                    $query->withCount('jobApplications')
+                          ->withAvg('jobApplications', 'expected_salary')
+                          ->with('employer'); // carrega employer junto
+                }
+            ])
+            ->latest()
+            ->get();
+
+        return view('my_job_application.index', [
+            'applications' => $applications
+        ]);
     }
 
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with(
+            'success',
+            'Job application removed.'
+        );
     }
 }
