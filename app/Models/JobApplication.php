a50e9320235103062582cb6673f45b79b83f3bb9
gabriel <gabrielmesquita0503@gmail.com>
2025-04-06 17:27:17 -0300

    File Uploads: Uploading Files

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -11,7 +11,7 @@ class JobApplication extends Model
     /** @use HasFactory<\Database\Factories\JobApplicationFactory> */
     use HasFactory;
 
-    protected $fillable = ['expected_salary', 'user_id', 'job_board_id'];
+    protected $fillable = ['expected_salary', 'user_id', 'job_board_id', 'cv_path'];
 
     public function job_board(): BelongsTo
     {
